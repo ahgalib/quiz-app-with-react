c6ca74155f2230bcc1a771bf4c9984043e4f3696
user name <asadullahalgalib783@gmail.com>
2022-03-05 22:38:07 +0600

    create files for the project

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+//firebase configaration
+const app = initializeApp({
+  apiKey: "AIzaSyC5217-82Pki-4vIIFf5Gn-NwZGK6JiV1E",
+  authDomain: "react-quiz-dev-c2121.firebaseapp.com",
+  projectId: "react-quiz-dev-c2121",
+  storageBucket: "react-quiz-dev-c2121.appspot.com",
+  messagingSenderId: "463533524696",
+  appId: "1:463533524696:web:33a37568d77312e941b9ad",
+});
+
+export default app;
